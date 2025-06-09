e3431850be9ef62fef2afc3ddce466f3223045e8
pmj8298 <164135340+pmj8298@users.noreply.github.com>
2025-06-09 17:06:19 +0900

    mapping

---
+++ CDN_react/cdn_r09.js
@@ -0,0 +1,7 @@
+const users = [
+  {id:1, title:'제목1',name:'쑤',date:'2025-08-31',count:22},
+  {id:2, title:'제목2',name:'꾸',date:'2025-12-07',count:32},
+  {id:3, title:'제목3',name:'뚜',date:'2025-06-09',count:42},
+  {id:4, title:'제목4',name:'뿌',date:'2025-08-24',count:72},
+  {id:5, title:'제목5',name:'쿠',date:'2025-02-17',count:62},
+]
